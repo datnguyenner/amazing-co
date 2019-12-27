c203ea9c5cb6bdc4414b0ce61ddbbb3dc214f56d
Dat Nguyen <datnguyen@dats-air.lan>
2019-12-27 01:35:07 -0800

    add changeParent and test

--- src/main/java/amazingco/JedisClient.java
+++ src/main/java/amazingco/JedisClient.java
@@ -10,6 +10,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class JedisClient {
 
@@ -24,21 +25,20 @@ public class JedisClient {
 	public void addNode(Node node) {
 		StringBuilder sb = new StringBuilder();
 		String key = sb.append(node.getName()).append(":root").toString();
-		System.out.println(key);
-		this.jedis.append(key, node.getRoot() != null ? node.getRoot() : "");
+		this.jedis.set(key, node.getRoot() != null ? node.getRoot() : "");
 
 		sb = new StringBuilder();
 		key = sb.append(node.getName()).append(":parent").toString();
-		this.jedis.append(key, node.getParent() != null ? node.getParent() : "");
+		this.jedis.set(key, node.getParent() != null ? node.getParent() : "");
 
 		sb = new StringBuilder();
 		key = sb.append(node.getName()).append(":height").toString();
-		this.jedis.append(key, String.valueOf(node.getHeight()));
+		this.jedis.set(key, String.valueOf(node.getHeight()));
 
 		sb = new StringBuilder();
 		final String listKey = sb.append(node.getName()).append(":children").toString();
 
-		node.getChildren().forEach(child -> this.jedis.lpush(listKey, child));
+		node.getChildren().forEach(child -> this.jedis.sadd(listKey, child));
 
 	}
 
@@ -47,11 +47,69 @@ public class JedisClient {
 		Node node = new Node(name, this.jedis.get(name + ":root"), this.jedis.get(name + ":parent"),
 				Integer.valueOf(this.jedis.get(name + ":height")));
 
-		List<String> childrens = this.jedis.lrange(name + ":children", 0, -1);
+		Set<String> childrens = this.jedis.smembers(name + ":children");
 		childrens.forEach(child -> node.addChild(child));
 
 		return node;
 		
+	}
+	
+	public void removeChild(String parent, String child) {
+
+		StringBuilder sb = new StringBuilder();
+		final String key = sb.append(parent).append(":children").toString();
+
+		this.jedis.srem(key, child);
+		
+	}
+	
+	public String getParent(String node) {
+
+		StringBuilder sb = new StringBuilder();
+		String key = sb.append(node).append(":parent").toString();
+
+		return jedis.get(key);
+		
+	}
+	
+	public void setParent(String child, String parent) {
+
+		StringBuilder sb = new StringBuilder();
+		String key = sb.append(child).append(":parent").toString();
+
+		this.jedis.set(key, parent);
+		
+	}
+	
+	public void setHeight(String child, int height) {
+
+		StringBuilder sb = new StringBuilder();
+		String key = sb.append(child).append(":height").toString();
+
+		this.jedis.set(key, String.valueOf(height));
+		
+	}
+	
+	public String getHeight(String node) {
+
+		StringBuilder sb = new StringBuilder();
+		String key = sb.append(node).append(":height").toString();
+
+		return this.jedis.get(key);
+		
+	}
+	
+	public void addChild(String parent, String child) {
+
+		StringBuilder sb = new StringBuilder();
+		String key = sb.append(parent).append(":children").toString();
+
+		this.jedis.sadd(key, child);
+		
+	}
+	
+	public void deleteAllKeys() {
+		this.jedis.flushAll();
 	}
 
 }
--- src/main/java/amazingco/NodeUtil.java
+++ src/main/java/amazingco/NodeUtil.java
@@ -8,10 +8,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class NodeUtil {
-	
-	JedisClient jedis = new JedisClient("127.0.0.1", 6379, "");
 
-	public List<Node> getDescendants(Node node) {
+	public List<Node> getDescendants(Node node, JedisClient jedis) {
 
 		List<Node> descendants = new ArrayList<>();
 
@@ -29,6 +27,36 @@ public class NodeUtil {
 		}
 
 		return descendants;
+	}
+
+	public void changeParent(String child, String newParent, JedisClient jedis) {
+
+		String currentParent = jedis.getParent(child);
+		jedis.removeChild(currentParent, child);
+
+		int height = Integer.valueOf(jedis.getHeight(newParent));
+		jedis.setParent(child, newParent);
+		jedis.setHeight(child, height + 1);
+		jedis.addChild(newParent, child);
+
+		Node childNode = jedis.getNode(child);
+		updateDescendantsHeight(childNode, jedis);
+
+	}
+
+	private void updateDescendantsHeight(Node node, JedisClient jedis) {
+
+		Deque<Node> nodes = new ArrayDeque<>();
+		nodes.push(node);
+
+		while (!nodes.isEmpty()) {
+			Node topNode = nodes.pop();
+			topNode.getChildren().forEach(child -> {
+				Node dataNode = jedis.getNode(child);
+				jedis.setHeight(child, topNode.getHeight() + 1);
+				nodes.push(dataNode);
+			});
+		}
 	}
 
 }
